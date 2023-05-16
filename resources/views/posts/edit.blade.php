c269426233149499be97d1244e0ed075e94509f2
titandaeru <sbf5963@gmail.com>
2023-05-16 19:00:42 +0900

    記事の編集

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -8,22 +8,21 @@
     </head>
     <body>
         <h1>Blog Name</h1>
-        <form action="/posts" method="POST">
+        <form action="/posts/{{ $post->id }}" method="POST">
             @csrf
+            @method('PUT')
             <div class="title">
                 <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル"/>
-                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+                <input type="text" name="post[title]" placeholder="タイトル" value={{ $post->title }}>
             </div>
             <div class="body">
                 <h2>Body</h2>
-                <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。"></textarea>
-                <p class="body__error" style="color:red">{{ $errors->first('post.body')}}</p>
+                <textarea name="post[body]" placeholder="今日も一日お疲れさまでした">{{ $post->body }}</textarea>
             </div>
-            <input type="submit" value="store"/>
+            <input type="submit" value="update">
         </form>
         <div class='footer'>
-            <a href="/">戻る</a>
+            <a href="/posts/{{ $post->id }}">戻る</a>
         </div>
     </body>
 </html>
