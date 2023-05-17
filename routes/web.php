ca0a16d8379c216dfdeacb246ad6cbbf71285a51
titandaeru <sbf5963@gmail.com>
2023-05-17 12:42:39 +0900

    ブログ投稿の削除

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/posts/create',[PostController::class,'create']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}',[PostController::class, 'show']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
-Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::put('/posts/{post}', [PostController::class, 'update']); 
+Route::delete('/posts/{post}', [PostController::class,'delete']);
